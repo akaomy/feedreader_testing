f72fbecb12a462fe9a1ecb4bb33a03886d9d45b2
akozlova <annakozlova92@gmail.com>
2019-06-07 12:41:33 -0700

    add test to check if feed is actually changes when new feed is loaded

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,5 +1,18 @@
 $(function() {
-    xdescribe('RSS Feeds', function() {
+    /* This suite is all about the RSS
+    * feeds definitions, the allFeeds variable in our application.
+    */
+
+    describe('RSS Feeds', function() {
+       /* This is our first test - it tests to make sure that the
+       * allFeeds variable has been defined and that it is not
+       * empty. */
+
+       /* TODO: Write a test that loops through each feed
+       * in the allFeeds object and ensures it has a URL defined
+       * and that the URL is not empty.
+       */
+
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
@@ -22,7 +35,13 @@ $(function() {
          });
     });
 
-    xdescribe("The menu", function() {
+    describe("The menu", function() {
+       /* Test that ensures the menu changes
+       * visibility when the menu icon is clicked. The menu
+       * should display when clicked and hide when clicked again.
+       * Tests if the menu-hidden class is removed after clicking the menu
+       * Then hides the menu again.
+       */
        let isHidden = document.querySelector('.menu-hidden').className;
        let menuIcon = document.querySelector('.menu-icon-link');
 
@@ -46,15 +65,20 @@ $(function() {
     });
 
     describe('Initial Entries', function() {
+     /* Test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     * Loads a feed then tests whether a feed (of class entry-link)
+     * has been loaded or not
+     */
       beforeEach(function(done) {
              loadFeed(2, done);
          });
 
-         it('will check if at least one entry exist and it\'s not empty once feed loads', function() {
+         it('have at least one entry exist and it\'s not empty once feed loads', function() {
              feed = document.querySelectorAll('.feed .entry');
              expect(feed.firstElementChild).not.toEqual(null);
              expect(feed.firstElementChild).not.toEqual(0);
-             console.log(feed.firstElementChild);
              expect(feed.length).toBeGreaterThan(0);
          });
 
@@ -65,11 +89,21 @@ $(function() {
        * by the loadFeed function that the content actually changes.
        * Remember, loadFeed() is asynchronous.
        */
-      // beforeEach(function (done){
-      //   loadFeed(2, done);
-      //   done();
-      // });
+      let prevFeed;
+      let nextFeed;
 
+         beforeEach(function(done) {
+            loadFeed(2, function() {
+                prevFeed = document.querySelector('.feed').innerText;
+                loadFeed(0, function() {
+                    nextFeed = document.querySelector('.feed').innerText;
+                    done();
+                });
+            });
+         });
 
+         it('changes content when a new feed is loaded', function() {
+             expect(prevFeed).not.toBe(nextFeed);
+         });
     });
 }());
